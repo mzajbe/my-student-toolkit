b262a9a58ce83324b8c20397ea9373cbbe36d85b
mzajbe <mzajbe@gmail.com>
2025-08-28 06:17:37 +0600

    shadcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-
-
-import './App.css'
+import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-
   return (
     <>
-      <h1 className='text-red-500'>hello</h1>
+      <h1 className="text-red-500">hello</h1>
+      <div>
+        <Button>Click me</Button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
